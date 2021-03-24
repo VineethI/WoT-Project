84c168b09b8154cf8401a3f727e4b74a06228702
VineethI <66237994+VineethI@users.noreply.github.com>
2021-03-23 19:20:41 -0500

    added app bar for dashboard

--- client/src/Components/Dashboard/Dashboard.jsx
+++ client/src/Components/Dashboard/Dashboard.jsx
@@ -1,10 +1,52 @@
 import React from "react";
 import "./Dashboard.scss";
 import Grid from "@material-ui/core/Grid";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import {Link} from 'react-router-dom'
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import IconButton from "@material-ui/core/IconButton";
+import AccountCircle from "@material-ui/icons/AccountCircle";
 
 const Dashboard = () => {
   return (
     <div className="dashboard">
+      <AppBar className="appBar" position="static">
+        <Toolbar className="toolbar" variant="dense">
+          <Typography variant="h6">
+            Environmental Monitoring Dashboard
+          </Typography>
+          <div className="logOut">
+            <Link to="/login">
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+            </Link>
+            {/* <Menu
+                id="menu-appbar"
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+              >
+                <MenuItem>Profile</MenuItem>
+                <MenuItem>My account</MenuItem>
+              </Menu> */}
+          </div>
+        </Toolbar>
+      </AppBar>
       <Grid className="gridTopLeft" container spacing={2}>
         <Grid className="leftc1" item md={7}>
           <div className="dashboardIntro">
@@ -18,9 +60,7 @@ const Dashboard = () => {
               <p>Outdoor Temperature</p>
               <p>Fuzzy Cloudy Weather</p>
             </div>
-            <div className="dashboardIntroImg">
-
-            </div>
+            <div className="dashboardIntroImg"></div>
           </div>
           <div className="dashboardIntroImg"></div>
         </Grid>
@@ -35,9 +75,7 @@ const Dashboard = () => {
       </Grid>
       <Grid className="gridMiddle" container spacing={2}>
         <Grid className="leftMid" item md={6}>
-          <div className="graph">
-            Graph 
-          </div>
+          <div className="graph">Graph</div>
         </Grid>
         <Grid className="gridMidRight" container md={6} spacing={3}>
           <Grid className="rightMid" item md={11}>
@@ -45,11 +83,9 @@ const Dashboard = () => {
           </Grid>
         </Grid>
       </Grid>
-      <Grid className="gridBottomLeft" container spacing={2}>
+      {/* <Grid className="gridBottomLeft" container spacing={2}>
         <Grid className="light" item md={4}>
-          <div className="dashboardIntro">
-            Light Intensity
-          </div>
+          <div className="dashboardIntro">Light Intensity</div>
         </Grid>
         <Grid className="gridBottomRight" container md={8} spacing={3}>
           <Grid className="bottomc1" item md={5}>
@@ -59,7 +95,7 @@ const Dashboard = () => {
             CO2 Emissions
           </Grid>
         </Grid>
-      </Grid>
+      </Grid> */}
     </div>
   );
 };
